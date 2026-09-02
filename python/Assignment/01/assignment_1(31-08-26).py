c0d3a5e4031a6546c398c4c1da9961d671edf0d7
mdmasoom866 <md.masoom.cg@gmail.com>
2026-09-02 22:16:57 +0530

    python folder created and o1 assignment done

---
+++ python/Assignment/01/assignment_1(31-08-26).py
@@ -0,0 +1,357 @@
+#Assignment
+Ans:1
+name = "Raju"
+print("Name:", name)
+
+Ans:2
+student_name = "Aman"
+print("Student Name:", Student_name)
+
+
+Ans:3
+student name = "Rahul"
+print("Student Name:", student name)
+
+Ans:4
+1student_name = "Ravi"
+print("Student Name:", 1student_name)
+
+Ans:5
+student-name = "Rohan"
+print("Student Name:", student-name)
+
+Ans:6
+student_name = "Neha"
+student_age = 19
+
+print("Name:", student_name)
+print("Age:", student_age)
+
+Ans:7
+Student_Name = "Karan"
+student_name = "Raj"
+
+print("Student 1:", Student_Name)
+print("Student 2:", student_name)
+
+Ans:8
+student_name = "Priya"
+student_age = 19
+
+print("Name:", student_name)
+print("Age:", student_age)
+
+Ans:9
+name = "Amit"
+student_age = 18
+
+print("Name:", name)
+print("Age:", student_age)
+
+Ans:10
+college_name = "ABC College"
+student_name = "Meera"
+
+print("College:", college_name)
+print("Student:", student_Name)
+
+Ans:11
+first_name = "Raj"
+last_name = "Patel"
+
+print("Full Name:", first_name, last_name)
+
+Ans:12
+first_name = "Raj"
+last_name = "Patel"
+
+print("Full Name:", first_name, last_name)
+
+Ans:13
+name="Rahul"
+age=18
+course="B.tech"
+city="patna"
+print("my name is ",name,".i am ",age,"years old .i am doing",course,"i lie in",city,".")
+
+Ans:14
+Name= "Aman Kumar"
+Roll_Number= 101
+Branch="computer science"
+Semester=1
+print("My name is",Name,"my roll number is",Roll_Number,"i am doing ",Branch,"now i am in",Semester,"1.")
+
+Ans:15
+Name="Priya Sharma"
+Age=19
+Gender="female" 
+City= "Jaipur"
+print("my name is",Name,"i am ",Age,"my gender is",Gender,"i live in",City,".")
+
+Ans:16
+Brand=" Samsung"
+Model="Galaxy A55"
+ram="8 GB"
+Storage= "256 GB"
+print("i have",Brand,Model,"with ram",ram,"of storage",Storage,".")
+
+Ans:17
+name = "Riya"
+age = 20
+marks = 85.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+
+Ans:22
+name = "Riya"
+age = 20
+marks = 85.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+
+Ans:23
+age = "18"
+marks = 85.5
+
+print("Age:", age)
+print("Marks:", marks)
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+
+Ans:24
+age = 18
+marks = "85.5"
+
+print("Age:", age)
+print("Marks:", marks)
+
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+
+Ans:25
+name = "Ravi"
+age = 18
+marks = 90.5
+passed = True
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+print("Passed:", passed)
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(passed))
+
+Ans:26
+name = "Ravi"
+age = 18
+marks = 90.5
+passed = True
+
+print("Name:", name)
+print("Age:", Age)
+print("Marks:", marks)
+print("Passed:", passed)
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(passed))
+
+Ans:27
+student_name = "Karan"
+student_age = 18
+student_marks = 87.5
+
+print("Student:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+
+print("Student Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+
+Ans:28
+student_name = "Karan"
+student_age = 18
+student_marks = 87.5
+
+print("Student:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+
+print("Student Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+
+Ans:29
+name = "Riya"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+Ans:30
+name = "Aman"
+age = 18
+
+print("Name:", name)
+print("Age:", age)
+
+Ans:31
+student_name = "Rohan"
+student_age = 19
+student_city = "Ahmedabad"
+
+print("Name:", (student_name))
+print("Age:", student_age)
+print("City:", student_city)
+
+Ans:32
+student_name = "Neha"
+student_age = 20
+student_city = "Vadodara"
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("City:", student_city)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("City Type:", type(student_city))
+
+Ans:33
+student_name = "Amit"
+student_age = 18
+student_college = "XYZ College"
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("College:", student_college)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+
+Ans:34
+student_name = "Meera"
+student_age = 20
+student_college = "ABC College"
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("College:", student_college)
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("College Type:", type(student_college))
+
+Ans:35
+student_name = "Raju"
+student_age = 18
+student_marks = 92.5
+is_passed = True
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+print("Passed:", is_passed)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+print("Passed Type:", type(is_passed))
+
+Ans:36
+student_name = "Raju"
+student_age = 18
+student_marks = 92.5
+is_passed = True
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+print("Passed:", is_passed)
+
+Ans:37
+name = "Amit"
+age = 18
+marks = 91.5
+
+print("Name:", Name)
+print("Age:", age)
+print("Marks:", marks)
+
+print("Name Type:", type(name))
+print("Age Type:", type(Age))
+print("Marks Type:", type(marks))
+
+Ans:38
+college_name = "ABC College"
+student_name = "Riya"
+student_age = 19
+
+print("College:", college_name)
+print("Student:", student_name)
+print("Age:", student_age)
+print("College Type:", type(college_name))
+
+Ans:39
+_class="B.Tech"
+student_name = "Ravi"
+print("class:",_class,)
+print("Student:", student_name)
+
+Ans:40
+student_name = "Ravi"
+student_age = 18
+student_marks = 89.5
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+
+Ans:41
+student_name = "Ravi"
+student_age = 18
+student_marks = 89.5
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+
+Ans:42
+name = "Karan"
+age = "18"
+marks = 87.5
+college_name = "ABC College"
+
+print=("Name:", name)
+print("Age:", Age)
+print("Marks:", marks)
+print("College:", college_name)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+print("College Type:", type(college_name))
+
+
